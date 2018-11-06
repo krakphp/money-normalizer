4e667427df591590191d8df33c3cf8a16ccb905d
RJ Garcia <rj@bighead.net>
2018-11-06 00:44:38 -0800

    Added support for denormalizing currency
    Signed-off-by: RJ Garcia <rj@bighead.net>

--- src/MoneyDenormalizer.php
+++ src/MoneyDenormalizer.php
@@ -9,10 +9,14 @@ use Symfony\Component\Serializer\Normalizer\DenormalizerInterface;
 class MoneyDenormalizer implements DenormalizerInterface
 {
     public function denormalize($data, $class, $format = null, array $context = array()) {
-        return new Money($data['amount'], new Currency($data['currency']));
+        if ($class === Money::class) {
+            return new Money($data['amount'], new Currency($data['currency']));
+        } else {
+            return new Currency($data);
+        }
     }
 
     public function supportsDenormalization($data, $type, $format = null) {
-        return $type === Money::class;
+        return $type === Money::class || $type === Currency::class;
     }
 }
